c9c06f9c5c5e1a4689b1fa9198173a76f861ea53
Renzo Kooi <renzo.kooi@gmail.com>
2020-12-15 14:53:19 +0100

    Allow empty etc

--- JQueryLike.js
+++ JQueryLike.js
@@ -29,28 +29,28 @@ import {
 const copyNativeProtos = ctor => {
   //copy Element prototype methods
   Object.entries(Object.getOwnPropertyDescriptors(Element.prototype))
-    .filter( propDescriptr => propDescriptr.value instanceof Function)
-    .forEach( ([key, { value }]) =>
-      ctor.prototype[key] = requestAnimationFrame( function(...args) {
+    .filter(propDescriptr => propDescriptr.value instanceof Function)
+    .forEach(([key, {value}]) =>
+      ctor.prototype[key] = requestAnimationFrame(function (...args) {
         return loop(this, elem => value.apply(elem, args));
-      }) );
+      }));
 
   // copy NodeList prototype methods
   Object.entries(Object.getOwnPropertyDescriptors(NodeList.prototype))
-    .filter( propDescriptr => propDescriptr.value instanceof Function)
-    .forEach( ([key]) => {
-        ctor.prototype[key] = function(lambda) {
-          this.collection[key](lambda);
-          return this;
-        };
-    } );
+    .filter(propDescriptr => propDescriptr.value instanceof Function)
+    .forEach(([key]) => {
+      ctor.prototype[key] = function (lambda) {
+        this.collection[key](lambda);
+        return this;
+      };
+    });
 }
 
 // the prototype initializer
 const initializePrototype = (ctor, extensions) => {
   // more complex prototype methods
   Object.entries(extensions).forEach(([key, lambda]) => {
-    ctor.prototype[key] = function(...args) {
+    ctor.prototype[key] = function (...args) {
       return lambda.fn
         ? lambda.fn(this, ...args)
         : loop(this, el => lambda(el, ...args));
@@ -63,69 +63,61 @@ const initializePrototype = (ctor, extensions) => {
 // -------------------------------------------------------------------- //
 const {$, util} = (() => {
   function ExtendedNodeList(
-      inputObject,
-      root = document.body,
-      position = insertPositions.BeforeEnd ) {
+    inputObject,
+    root = document.body,
+    position = insertPositions.BeforeEnd) {
 
     if (ExtendedNodeList.prototype.isSet === undefined) {
       initializePrototype(ExtendedNodeList, extensions);
     }
 
     this.collection = [];
-    const cleanupAndAppendCollection = () => this.collection = this.collection.reduce( (acc, elem) =>
+    const cleanupAndAppendCollection = () => this.collection = this.collection.reduce((acc, elem) =>
       !elem.dataset.elementInvalid && [...acc, element2DOM(elem, root, position)] || acc, []
     );
 
     const selectorRoot = root !== document.body &&
-      (inputObject.constructor === String &&
-        inputObject.toLowerCase() !== "body") ? root : document;
+    (inputObject.constructor === String &&
+      inputObject.toLowerCase() !== "body") ? root : document;
 
     try {
       const isArray = Array.isArray(inputObject);
-
-      if (inputObject) {
-        if (inputObject instanceof HTMLElement) {
-          this.collection = [inputObject];
-        } else if (inputObject instanceof NodeList) {
-          this.collection = [...inputObject];
-        } else if(inputObject instanceof ExtendedNodeList) {
-          this.collection = inputObject.collection;
-        } else if ( isArray || `${inputObject}`.trim().startsWith("<") ) {
-          if (logStatus()) {
-            log(`trying to create ... [${inputObject}]`);
-          }
-
-          if (isArray) {
-            inputObject.forEach( html => {
-               const nwElem = createElementFromHtmlString( html, root );
-               this.collection.push( nwElem.childNodes[0] );
-            });
-          } else {
-            const nwElem = createElementFromHtmlString(inputObject.trim(), root);
-            this.collection = [ nwElem.childNodes[0] ];
-          }
-          // remove erroneous elems and append to DOM
-          cleanupAndAppendCollection();
-
-          if (logStatus()) {
-            log(`created element: *clean: [${this.collection[0].outerHTML}]`);
-          }
-        } else if (inputObject.constructor === String) {
-          this.collection = [ ...selectorRoot.querySelectorAll(inputObject) ];
-        } else {
-          throw new TypeError(`input [${inputObject}] not valid`);
-        }
-        return this;
-      } else {
-        throw new TypeError(`ExtCollextion init: no input`);
+      if (!inputObject) {
+        this.collection = [];
       }
+      if (inputObject instanceof HTMLElement) {
+        this.collection = [inputObject];
+      } else if (inputObject instanceof NodeList) {
+        this.collection = [...inputObject];
+      } else if (inputObject instanceof ExtendedNodeList) {
+        this.collection = inputObject.collection;
+      } else if (isArray || `${inputObject}`.trim().startsWith("<") && `${inputObject}`.trim().endsWith(">")) {
+        log(`trying to create ... [${inputObject}]`);
+
+        if (isArray) {
+          inputObject.forEach(html => {
+            const nwElem = createElementFromHtmlString(html, root);
+            this.collection.push(nwElem.childNodes[0]);
+          });
+        } else {
+          const nwElem = createElementFromHtmlString(inputObject.trim(), root);
+          this.collection = [nwElem.childNodes[0]];
+        }
+        // remove erroneous elems and append to DOM
+        cleanupAndAppendCollection();
+
+        log(`created element: *clean: [${this.collection[0].outerHTML}]`);
+      } else if (inputObject.constructor === String) {
+        this.collection = [...selectorRoot.querySelectorAll(inputObject)];
+      } else {
+        throw new TypeError(`input [${inputObject}] not valid`);
+      }
+      return this;
     } catch (err) {
       const msg = `Caught jql selector or html error:\n${err.stack ? err.stack : err.message}`;
-      if (logStatus()) {
-        log(msg);
-      } else {
-        console.log(msg);
-      }
+      log(msg);
+    //^ only if logStatus = on, so
+      console.log(msg);
     }
   }
 
@@ -139,7 +131,8 @@ const {$, util} = (() => {
       allowUnknownHtmlTags,
       insertPositions,
       getRestricted,
-      closestSibling }
+      closestSibling
+    }
   };
 })();
 
