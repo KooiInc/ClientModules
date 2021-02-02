530fe1b0941eef41912306451c39903c7679f0a7
Renzo Kooi <renzo.kooi@gmail.com>
2021-02-02 14:31:10 +0100

    logging

--- Log.js
+++ Log.js
@@ -9,8 +9,8 @@ const log = (...args) => {
       const loggingFieldSet = `
       <fieldset style="border: 1px dotted rgb(153, 153, 153); max-width: 800px;">
         <legend style="text-align: center; background-color: rgb(119, 119, 119); padding: 2px 5px; 
-            margin-top: -0.5rem; color: white; font-family: 'verdana, arial'">Logging</legend>
-        <pre id="jql_logger" style="max-width: 800px; max-height: 600px; overflow: auto; white-space: pre-line; margin: 0px;"></pre>
+            margin-top: -0.5rem; color: white; font: normal 12px/15px verdana, arial">Logging</legend>
+        <pre id="jql_logger" style="max-width: 800px; max-height: 600px; overflow: auto; margin: 0px;"></pre>
       </fieldset>`;
       const logBlock = createElementFromHtmlString(loggingFieldSet, document.body);
       document.body.insertBefore(logBlock, document.body.childNodes[0]);
