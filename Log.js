52f237650779c513e21f36ad24d05dcc6fbebbbc
Renzo Kooi <renzo.kooi@gmail.com>
2021-02-02 14:22:27 +0100

    logging

--- Log.js
+++ Log.js
@@ -1,26 +1,30 @@
-import { createElementFromHtmlString } from "./DOM.js";
+import {createElementFromHtmlString, insertPositions} from "./DOM.js";
 
 let useLogging = false;
-const debugLog = { on: () => useLogging = true, off: () => useLogging = false, };
+const debugLog = {on: () => useLogging = true, off: () => useLogging = false,};
 const log = txt => {
-  if (!useLogging) { return; }
-  if (!document.querySelector("#jql_logger")) {
-    const logBlock = createElementFromHtmlString(`<pre id="jql_logger"></pre>`, document.body);
-    Object.entries( {
-         maxHeight: "200px",
-         overflow: "auto",
-         position: "fixed",
-         bottom: "0.5rem",
-         left: "1rem",
-         right: "1rem",
-         padding: "0.5rem 1rem",
-         maxWidth: "inherit",
-         border: "1px dotted #777",
-      } ).forEach( ([key, value]) => logBlock.style[key] = value);
-    document.body.appendChild(logBlock);
-  }
-  document.querySelector("#jql_logger").textContent += `.${txt}\n`;
-};
-const logStatus = () => useLogging;
+    if (!useLogging) { return; }
 
+    if (!document.querySelector("#jql_logger")) {
+      const loggingFieldSet = `
+      <fieldset style="border: 1px dotted rgb(153, 153, 153); max-width: 800px;">
+        <legend style="text-align: center; background-color: rgb(119, 119, 119); padding: 2px 5px; 
+            margin-top: -0.5rem; color: white;">Logging</legend>
+        <pre id="jql_logger" style="max-width: 800px; max-height: 600px; overflow: auto; white-space: pre-line; margin: 0px;"></pre>
+      </fieldset>`;
+      const logBlock = createElementFromHtmlString(loggingFieldSet, document.body);
+      document.body.insertBefore(logBlock, document.body.childNodes[0]);
+    }
+
+    document.querySelector("#jql_logger").textContent += `.${txt}\n`;
+  };
+
+const logStatus = () => useLogging;
 export {log, debugLog, logStatus};
+
+/*
+<fieldset style="border: 1px dotted rgb(153, 153, 153); max-width: 800px;">
+  <legend style="text-align: center; background-color: rgb(119, 119, 119); padding: 2px 5px; margin-top: -0.5rem; color: white;">Logging</legend>
+  <pre id="log" style="max-width: 800px; max-height: 600px; overflow: auto; white-space: pre-line; margin: 0px;"></pre>
+</fieldset>
+ */
